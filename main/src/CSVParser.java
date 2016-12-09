532b2a1616e4047f9799d7b4beded787c8b242eb
Allen <allen.lovett@hotmail.co.uk>
2016-12-09 17:48:37 +0000

    Added initial CSV processing functionality

--- main/src/CSVParser.java
+++ main/src/CSVParser.java
@@ -1,5 +1,9 @@
 import data.Player;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -9,10 +13,37 @@ public class CSVParser {
 
     public List<Player> players;
 
-    public void getPlayers() {
+    public static void parsePlayers(){
 
-        // Read the Player CSV file and create objects for the players
+        String csvFilePath = "/Users/allen/IdeaProjects/5-a-side-manager/main/src/csv/players.csv";
+        BufferedReader bufferedReader = null;
+        String csvToken = "\\,";
+        String line = "";
+
+        try{
+            bufferedReader = new BufferedReader(new FileReader(csvFilePath));
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] players = line.split(csvToken);
+                System .out.println("ID: "+players[0]+"\nName: "+players[1]+"\nBalance: "+players[2]);
+            }
+
+        }catch (FileNotFoundException e){
+            System.out.println("Error: "+e);
+        }
+        catch (IOException e){
+            System.out.println("Error: "+e);
+        }
+    }
+
+    public void getPlayerList() {
+
+        // Read the Player CSV file and generate a list of Player names
+        // This will return the name of the Player along with their ID
 
 
+    }
+
+    public static void main(String[] args) {
+        parsePlayers();
     }
 }
